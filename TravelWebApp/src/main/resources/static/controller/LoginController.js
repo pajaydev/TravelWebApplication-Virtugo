39244f2956cc84a0ae6d323452047f6be2a53ea4
ajay2507 <pajay2507@gmail.com>
2016-10-26 17:34:18 -0400

    Included login and registration.

---
+++ TravelWebApp/src/main/resources/static/controller/LoginController.js
@@ -0,0 +1,6 @@
+travelApp.controller("loginController",function($scope){
+	alert("login controller");
+	$scope.loginFlag = true;
+	//alert(loginFlag);
+	
+})
--- TravelWebApp/src/main/resources/static/directives/route.js
+++ TravelWebApp/src/main/resources/static/directives/route.js
@@ -1,10 +1,38 @@
-var travelApp = angular.module("TravelApp",["ngRoute"]);
+var travelApp = angular.module("TravelApp",["ngRoute","ui.bootstrap"]);
+alert("route");
 travelApp.config(function($routeProvider){
 	$routeProvider.
-	when("/login",{
-		templateUrl:"templates/login.html"
+	when("/home",{
+		templateUrl:"templates/home.html"
+	})
+	.when("/index",{
+		templateUrl:"templates/home.html",
+		controller:"baseController"
+	})
+	.when("/login",{
+		templateUrl:"templates/login.html",
+		controller:"loginController"
 	})
 	.otherwise({
-		redirectTo:"/login"
+		redirectTo:"/home"
 	});
+});
+
+
+travelApp.controller("baseController",function($scope,$location,$route){
+	alert("&&&&&&&&&&&&");
+	var template = "";
+	$scope.login = function(){
+		alert("openpopup");
+		/*$scope.loginForm.$setPristine();
+		 $scope.registerForm.$setPristine();*/
+		 alert($location.path());
+		 if($location.path() == "/login"){
+			 $route.reload();
+		 }else{
+		$location.path('/login');}
+		 
+		 
+	}
+	
 });
